f4cd33156e3169e1b562c7e8fcda4ed759c4d71c
Mathias-Schuster <mathias.schuster.dev@proton.me>
2026-08-24 22:15:20 +0200

    Initial commit: Setup Spring Boot backend and React frontend

---
+++ backend/src/main/java/com/github/mathschu/volvisviewer/VolvisviewerApplication.java
@@ -0,0 +1,13 @@
+package com.github.mathschu.volvisviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolvisviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolvisviewerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/mathschu/volvisviewer/VolvisviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mathschu.volvisviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolvisviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
